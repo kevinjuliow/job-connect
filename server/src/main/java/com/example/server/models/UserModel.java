791e81b01525540f117270a3246aa2c2764ae91d
Kevin Ju <kevjuliow@gmail.com>
2024-06-07 21:31:09 +0700

    Update UserModel.java

---
+++ server/src/main/java/com/example/server/models/UserModel.java
@@ -0,0 +1,12 @@
+package com.example.server.models;
+
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class UserModel {
+}
--- server/src/main/java/com/example/server/models/userModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.server.models;
-
-public class userModel {
-}
