dd1f661e2f9bbb4f61fb5e77f6ce7a5e6d888b6c
Kevin Ju <kevjuliow@gmail.com>
2024-06-07 21:35:33 +0700

    Update ApplicantsModel.java

--- server/src/main/java/com/example/server/models/ApplicantsModel.java
+++ server/src/main/java/com/example/server/models/ApplicantsModel.java
@@ -1,10 +1,7 @@
 package com.example.server.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +13,8 @@ public class ApplicantsModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_applicant;
 
+    @Column(length = 50)
     private String email ;
 
-
+    
 }
