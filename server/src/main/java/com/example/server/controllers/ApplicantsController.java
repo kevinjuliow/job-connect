ce38d7716a21376cea75af20e26698d9f30f27dc
Kevin Ju <kevjuliow@gmail.com>
2024-06-08 00:09:14 +0700

    Applicants Updated

--- server/src/main/java/com/example/server/controllers/ApplicantsController.java
+++ server/src/main/java/com/example/server/controllers/ApplicantsController.java
@@ -42,6 +42,24 @@ public class ApplicantsController {
         ApplicantsModel addedApplicants = applicantsService.POST(body);
         return ResponseEntity.status(200).body(new ApplicantDtos("OK" , 200 , addedApplicants));
     }
-    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> PUT (@RequestBody @Valid ApplicantsModel body , @PathVariable Integer id , Errors e){
+        if (e.hasErrors()) return ResponseEntity.status(400).body(new ApplicantDtos("Bad Request" , 400 , ((List<ApplicantsModel>) null)));
+        try{
+            ApplicantsModel updated = applicantsService.PUTByID(body , id);
+            return ResponseEntity.status(200).body(new ApplicantDtos("OK" , 200 , updated));
+        }catch (UserNotFound err){
+            return ResponseEntity.status(404).body(new ApplicantDtos("Not Found" , 404 , ((List<ApplicantsModel>) null)));
+        }
+    }
+    @DeleteMapping
+    public ResponseEntity<?> DELETE (@PathVariable Integer id){
+        try{
+            applicantsService.DELETEByID(id);
+            return ResponseEntity.status(200).body(new ApplicantDtos("OK" , 200 , ((ApplicantsModel) null)));
+        }catch (UserNotFound err){
+            return ResponseEntity.status(404).body(new ApplicantDtos("Not Found" , 404 , ((List<ApplicantsModel>) null)));
+        }
+    }
 
 }
--- server/src/main/java/com/example/server/models/ApplicantsModel.java
+++ server/src/main/java/com/example/server/models/ApplicantsModel.java
@@ -11,6 +11,7 @@ import java.util.Date;
 @Entity
 @NoArgsConstructor
 @Data
+@Table(schema = "applicants")
 public class ApplicantsModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
